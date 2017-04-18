6e9003f4e3bd299f81c0aad32d35b680395cdaae
lihuabukai <wangyali@wangyali.local>
2017-04-18 16:13:03 +0800

    init

---
+++ Wang/Wang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wang
+//
+//  Created by 王亚丽 on 2017/4/18.
+//  Copyright © 2017年 adinno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wang/Wang/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Wang
+//
+//  Created by 王亚丽 on 2017/4/18.
+//  Copyright © 2017年 adinno. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
